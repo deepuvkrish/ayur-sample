2fc424987b199e98a75ae2820db0056a70c63ef8
Deepak K V <deepak.k@skillassure.com>
2024-07-18 15:09:28 +0530

    first push with base code template

---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,10 @@
+const users = [
+    {
+      id: '410544b2-4001-4271-9855-fec4b6a6442a',
+      name: 'test',
+      email: 'test@ayur.com',
+      password: '123456',
+    },
+  ];
+  
+  export { users };
